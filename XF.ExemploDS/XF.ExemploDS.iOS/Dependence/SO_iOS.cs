0d3d05d3f1812a32e48a84c7b763fbcaa3cf5f5d
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-31 21:16:34 -0200

    Add project files.

---
+++ XF.ExemploDS/XF.ExemploDS.iOS/Dependence/SO_iOS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using XF.ExemploDS.App_Code;
+using XF.ExemploDS.iOS.Dependence;
+
+[assembly: Dependency(typeof(SO_iOS))]
+namespace XF.ExemploDS.iOS.Dependence
+{
+    public class SO_iOS : ISistemaOperacional
+    {
+        public string Plataforma()
+        {
+            return "iPhone iOS";
+        }
+    }
+}
